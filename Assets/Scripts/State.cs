916ae0464c319a56cb2121983e6a6dd288199320
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2019-02-17 23:21:06 +0800

    Add some base state logic and constructs

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace State {
+
+    enum Player { A, B }
+
+    struct Piece
+    {
+        public enum Type { SWORD, SPEAR, HORSE }
+        public Player owner;
+        public int health;
+        public int attack;
+        public int def;
+        public Type type;
+
+        public string id; // unique id for a piece
+        public Square currentSquare;
+    }
+
+    struct Move
+    {
+        public enum Direction { UP, DOWN, LEFT, RIGHT, NONE }
+        public Piece piece;
+        public Direction direction;
+    }
+
+    struct Square
+    {
+        public int row;
+        public int col;
+        public Square(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+        public override int GetHashCode()
+        {
+            return row * 31 + col;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            return this == (Square) obj;
+        }
+
+        public static bool operator ==(Square a, Square b)
+        {
+            return a.row == b.row && b.col == b.col;
+        }
+
+        public static bool operator !=(Square a, Square b)
+        {
+            return !(a == b);
+        }
+    }
+
+    class Board
+    {
+        int numRows;
+        int numCols;
+        List<Piece> pieces;
+
+        public Board(int rows, int cols)
+        {
+            rows = numRows;
+            cols = numCols;
+        }
+
+        public Square NextSquare(Square currentSquare, Move.Direction direction)
+        {
+            int row, col;
+            switch (direction)
+            {
+                case Move.Direction.UP:
+                    row = currentSquare.row > 0 ? currentSquare.row - 1 : 0;
+                    return new Square(row, currentSquare.col);
+                case Move.Direction.DOWN:
+                    row = currentSquare.row < numRows -1 ? currentSquare.row + 1 : numRows - 1;
+                    return new Square(row, currentSquare.col);
+                case Move.Direction.LEFT:
+                    col = currentSquare.col > 0 ? currentSquare.col - 1 : 0;
+                    return new Square(currentSquare.row, col);
+                case Move.Direction.RIGHT:
+                    col = currentSquare.col < numCols - 1 ? currentSquare.col + 1 : numCols - 1;
+                    return new Square(currentSquare.row, col);
+            }
+            return currentSquare;
+        }
+
+        public Square NextSquare(Move move)
+        {
+            Square currentSquare = move.piece.currentSquare;
+            Move.Direction direction = move.direction;
+            return NextSquare(currentSquare, direction);
+        }
+    }
+
+}
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using State;
+using System;
+using System.Linq;
+
+public class StateManager : MonoBehaviour {
+    Board board = new Board(16, 16);
+    HashSet<Square> lockedSquares = new HashSet<Square>();
+
+    void ResetLockedSquares()
+    {
+        lockedSquares = new HashSet<Square>();
+    }
+
+    void LockSquares(IEnumerable<Square> squares)
+    {
+        foreach (Square square in squares)
+            LockSquare(square);
+    }
+
+    void LockSquare(Square square)
+    {
+        lockedSquares.Add(square);
+    }
+
+    bool IsSquareLocked(Square square)
+    {
+        return lockedSquares.Contains(square);
+    }
+
+    void CalculateNextState()
+    {
+    }
+
+    void ResolveMovement(Move[] moves)
+    {
+        ResetLockedSquares();
+        Square[] squaresWithStationaryPieces =
+            moves.Where(move => move.direction == Move.Direction.NONE)
+                 .Select(move => move.piece.currentSquare)
+                 .ToArray();
+
+        LockSquares(squaresWithStationaryPieces);
+
+        Tuple<Move, Square>[] movesToConsider =
+            moves.Where(move => move.direction != Move.Direction.NONE)
+                 .Select(move => Tuple.Create(move, board.NextSquare(move)))
+                 .ToArray();
+
+        HashSet<Move> potentialBounces = new HashSet<Move>();
+        HashSet<Move> movesRemoved = new HashSet<Move>();
+        bool hasInvalidMoves = true;
+
+        while (hasInvalidMoves)
+        {
+
+            for (int i = 0; i < movesToConsider.Length; i++)
+            {
+                Tuple<Move, Square> moveAndNextSquare = movesToConsider[i];
+                Move move = moveAndNextSquare.Item1;
+                Square nextSquare = moveAndNextSquare.Item2;
+
+                // Consider entering a locked square
+                bool isNextSquareLocked = IsSquareLocked(nextSquare);
+                if (isNextSquareLocked)
+                {
+                    LockSquare(move.piece.currentSquare);
+                    movesRemoved.Add(move);
+                    continue;
+                }
+
+                // Consider all conflicting moves to the next square
+                for (int j = i + 1; j < movesToConsider.Length; j++)
+                {
+                    Tuple<Move, Square> otherMoveAndNextSquare = movesToConsider[i];
+                    Move otherMove = otherMoveAndNextSquare.Item1;
+                    Square otherNextSquare = otherMoveAndNextSquare.Item2;
+
+                    // Consider bounces
+                    if (nextSquare == otherNextSquare)
+                    {
+                        LockSquare(move.piece.currentSquare);
+                        LockSquare(otherMove.piece.currentSquare);
+                        potentialBounces.Add(move);
+                        potentialBounces.Add(otherMove);
+                        movesRemoved.Add(move);
+                        movesRemoved.Add(otherMove);
+                    }
+
+                    // Consider Opposing movement
+                    if (nextSquare == otherMove.piece.currentSquare &&
+                        move.piece.currentSquare == otherNextSquare)
+                    {
+                        LockSquare(move.piece.currentSquare);
+                        LockSquare(otherMove.piece.currentSquare);
+                        movesRemoved.Add(move);
+                        movesRemoved.Add(otherMove);
+                    }
+                }
+            }
+
+            int numConsideredMove = movesToConsider.Length;
+            movesToConsider = movesToConsider
+                .Where(moveAndNextMove => !movesRemoved.Contains(moveAndNextMove.Item1)).ToArray();
+
+            hasInvalidMoves = numConsideredMove != movesToConsider.Length;
+        }
+
+        // apply valid moves
+
+        // find out the real bounces
+
+        // return
+
+
+    }
+
+    void LockSquares(Square[] square)
+    {
+
+    }
+
+    void IsInvalidMove()
+    {
+
+    }
+
+
+    void ResolveCombat()
+    {
+
+    }
+}
